68272d46662df374be874f2672e7cbf4ac130fdf
Brian Peiris <brianpeiris@gmail.com>
2020-01-11 17:40:20 -0800

    color filtering

--- src/main.py
+++ src/main.py
@@ -5,23 +5,37 @@ from mouse import MouseHandler
 from quad import Quad
 from store import Store
 
+store = Store('store.json')
+
 cv.namedWindow('input', cv.WINDOW_GUI_NORMAL | cv.WINDOW_AUTOSIZE)
+cv.moveWindow('input', 100, 500)
 cv.namedWindow('output', cv.WINDOW_GUI_NORMAL | cv.WINDOW_AUTOSIZE)
+cv.moveWindow('output', 750, 500)
+cv.namedWindow('filtered', cv.WINDOW_GUI_NORMAL | cv.WINDOW_AUTOSIZE)
+cv.moveWindow('filtered', 1250, 500)
 
 capture = cv.VideoCapture(4)
-
-store = Store('store.json')
 
 
 def updateStorage():
     store.put('quad', quad.getPoints())
 
 
-mouseHandler = MouseHandler('input')
-quad = Quad(store.get('quad'), mouseHandler, updateStorage)
+quad = Quad(store.get('quad'), MouseHandler('input'), updateStorage)
+
 outputRes = 480
 outputSize = (outputRes, outputRes)
 squarePoints = ((0, 0), (outputRes, 0), (0, outputRes), (outputRes, outputRes))
+lowerRed = np.array([145,100,20])
+upperRed = np.array([195,180,250])
+#lowerRed = np.array([100,60,20])
+#upperRed = np.array([130,180,250])
+kernel = np.ones((10, 10),np.uint8)
+
+def printColor(x, y):
+    print(hsv[y][x])
+
+MouseHandler('output').onMouseDown = printColor
 
 while True:
     ret, frame = capture.read()
@@ -30,8 +44,16 @@ while True:
     output = cv.warpPerspective(frame, warpMatrix, outputSize)
     cv.imshow('output', output)
 
+    hsv = cv.cvtColor(output, cv.COLOR_BGR2HSV)
+    filtered = cv.inRange(hsv, lowerRed, upperRed)
+    filtered = cv.dilate(filtered, kernel, iterations = 1)
+    cv.imshow('filtered', filtered)
+
     quad.update(frame)
     cv.imshow('input', frame)
 
     if cv.waitKey(1) == ord('q'):
         break
+
+cv.destroyAllWindows()
+capture.release()
--- src/mouse.py
+++ src/mouse.py
@@ -4,6 +4,7 @@ import cv2 as cv
 class MouseHandler:
     def __init__(self, window):
         self.state = [0, 0, 0]
+        self.onMouseDown = None
         cv.setMouseCallback(window, self.onMouse)
 
     def onMouse(self, event, x, y, flags, param):
@@ -12,5 +13,7 @@ class MouseHandler:
             self.state[1] = y
         if event == cv.EVENT_LBUTTONDOWN:
             self.state[2] = 1
+            if self.onMouseDown is not None:
+                self.onMouseDown(x, y)
         if event == cv.EVENT_LBUTTONUP:
             self.state[2] = 0
